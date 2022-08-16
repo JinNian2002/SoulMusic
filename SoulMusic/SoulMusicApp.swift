039366672738986cfd5d458d0a4cea05b3774b56
ZZZS <1316755935@qq.com>
2022-08-16 22:39:45 +0800

    Fixed WelcomeView

--- SoulMusic/SoulMusicApp.swift
+++ SoulMusic/SoulMusicApp.swift
@@ -13,7 +13,7 @@ struct SoulMusicApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(Model())
         }
